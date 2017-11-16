f0a32df853658f22c45c20d97086aeedd143d0f8
Mudunuri, Sandeep <samudunu@servicemaster.com>
2017-11-16 16:14:12 -0600

    added projcet

---
+++ Helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+        public static string CreateMessage()
+        {
+            return "Helloworld";
+        }
+    }
+}
